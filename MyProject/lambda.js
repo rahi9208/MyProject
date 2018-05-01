8537c49b028147397168f9b79edc26804d79a409
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-05-01 12:44:20 +0530

    B

--- MyProject/lambda.js
+++ MyProject/lambda.js
@@ -3,5 +3,5 @@ let uuid = require('uuid');
 exports.handler = function (event, context, callback) {
 
 
-	callback(null, 'Successfully executed B' + uuid.v4());
+	callback(null, 'Successfully executed BXY' + uuid.v4());
 }
